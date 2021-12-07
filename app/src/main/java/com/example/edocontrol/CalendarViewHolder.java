391c70a0c74d286179cae4e9e03941624e12f5ee
JesperOja <jesper.oja@metropolia.fi>
2021-12-07 13:30:44 +0200

    Paljon muutoksia kalenteriin

---
+++ app/src/main/java/com/example/edocontrol/CalendarViewHolder.java
@@ -0,0 +1,30 @@
+package com.example.edocontrol;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    private final ArrayList<LocalDate> daysOfMonth;
+    public final View parentView;
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener, ArrayList<LocalDate> daysOfMonth) {
+        super(itemView);
+        this.daysOfMonth = daysOfMonth;
+        parentView = itemView.findViewById(R.id.parentView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        onItemListener.onItemClick(getAdapterPosition(),daysOfMonth.get(getAdapterPosition()));
+    }
+}
--- app/src/main/java/com/example/edocontrol/MainActivity.java
+++ app/src/main/java/com/example/edocontrol/MainActivity.java
@@ -1,106 +1,113 @@
 package com.example.edocontrol;
 
+import static com.example.edocontrol.CalendarUtils.daysInMonthArray;
+import static com.example.edocontrol.CalendarUtils.monthYearFromDate;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
+
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-import java.text.SimpleDateFormat;
-
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
 
 
-        // Define the variable of CalendarView type
-        // and TextView type;
-        CalendarView calendar;
-        TextView date_view;
+        private RecyclerView calendarRecyclerView;
+        private TextView monthYear;
         private ConstraintLayout mBottomSheetLayout;
         private BottomSheetBehavior sheetBehavior;
         private TextView bottomSheetDate;
         private ImageView bottomSheetImg;
         private Button addButton;
-        Date date = new Date();
-        SimpleDateFormat thisday = new SimpleDateFormat("EEEE");
-        String calendarDay;
+
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            bottomSheetDate = findViewById(R.id.BottomSheetDate);
-            mBottomSheetLayout = findViewById(R.id.bottom_sheet_layout);
-            sheetBehavior = BottomSheetBehavior.from(mBottomSheetLayout);
-            bottomSheetImg = findViewById(R.id.bottomSheetImg);
-            addButton = findViewById(R.id.addButton);
-            sheetBehavior.setExpandedOffset(80);
-            calendar = findViewById(R.id.calendar);
-            date_view = findViewById(R.id.date_view);
+            initWidget();
+            CalendarUtils.selectedDate = LocalDate.now();
+            setMonthView();
+            bottomSheetDate.setText(CalendarUtils.selectedDate.getDayOfWeek()+ " "+ CalendarUtils.selectedDate.getDayOfMonth() + " of " +monthYearFromDate(CalendarUtils.selectedDate));
 
-            date_view.setText(thisday.format(date) + " " +date.getDate() + "." +date.getMonth() + "." + date.getYear());
-
-            calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-
-                @Override
-                public void onSelectedDayChange(
-                        @NonNull CalendarView view,
-                        int year,
-                        int month,
-                        int dayOfMonth) {
-
-
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.set(year,month,dayOfMonth);
-                    int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-                    //Log.d("Date", thisday.format(dayofweek));
-                    switch (dayOfWeek){
-                        case 1: calendarDay = "Sunday";break;
-                        case 2: calendarDay = "Monday";break;
-                        case 3: calendarDay = "Tuesday";break;
-                        case 4: calendarDay = "Wednesday";break;
-                        case 5: calendarDay = "Thursday";break;
-                        case 6: calendarDay = "Friday";break;
-                        case 7: calendarDay = "Saturday";break;
-                    }
-                    String Date
-                            =  calendarDay + " " +dayOfMonth + "."
-                            + (month + 1) + "." + year;
-                    if(sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED){
-                        sheetBehavior.setHalfExpandedRatio((float) 0.35);
-                        sheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
-                        bottomSheetDate.setText(Date);
-                    }
-                    // set this date in TextView for Display
-                    date_view.setText(Date);
-                }
-            });
-            sheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-                @Override
-                public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                    bottomSheetImg.setAlpha(255);
-                }
-                @Override
-                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                    bottomSheetImg.setAlpha(100);
-
-                }
-            });
         }
 
+
+
+    private void initWidget() {
+        bottomSheetDate = findViewById(R.id.BottomSheetDate);
+        mBottomSheetLayout = findViewById(R.id.bottom_sheet_layout);
+        sheetBehavior = BottomSheetBehavior.from(mBottomSheetLayout);
+        bottomSheetImg = findViewById(R.id.bottomSheetImg);
+        addButton = findViewById(R.id.addButton);
+        sheetBehavior.setExpandedOffset(80);
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYear = findViewById(R.id.monthYearTV);
+    }
+    private void setMonthView() {
+            monthYear.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+
+
+
+    public void previousMonthAction(View view) {
+
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+            setMonthView();
+    }
+
+    public void nextMonthAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+            setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+            CalendarUtils.selectedDate = date;
+            setMonthView();
+        if(sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED){
+            sheetBehavior.setHalfExpandedRatio((float) 0.20);
+            sheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+            bottomSheetDate.setText(date.getDayOfMonth() +" of " +monthYearFromDate(CalendarUtils.selectedDate));
+        }
+
+        sheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                bottomSheetImg.setAlpha(255);
+            }
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                bottomSheetImg.setAlpha(100);
+
+            }
+        });
+    }
 }
