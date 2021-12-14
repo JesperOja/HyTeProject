2bed375a12c47db1ec5e135407c5689c8427d227
Anat0lis <77218737+Anat0lis@users.noreply.github.com>
2021-12-14 22:52:58 +0200

    LoginActivity pieni muutos

--- app/src/main/java/com/example/edocontrol/LoginActivity.java
+++ app/src/main/java/com/example/edocontrol/LoginActivity.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
